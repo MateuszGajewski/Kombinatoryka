48756241b698d8f351605669cd8490d7a8b8e984
arabica <mg96272@gmail.com>
2022-03-01 14:18:58 +0100

    "instances folder"

---
+++ Zadanie1/instance_parser/euclidean_parser.py
@@ -0,0 +1,7 @@
+from abc import ABC
+from Zadanie1.instance_parser.instance_parser import Parser
+
+
+class EuclideanParser(Parser, ABC):
+    def parse(self, filename=str):
+        return 0
--- Zadanie1/main.py
+++ Zadanie1/main.py
@@ -1,1 +1,4 @@
+from instance_parser import euclidean_parser
+
 print("A")
+p = euclidean_parser.EuclideanParser()
