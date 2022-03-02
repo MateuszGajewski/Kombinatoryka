bf80adb60a6c2dd839f17f8c8c116c52b241d69e
arabica <mg96272@gmail.com>
2022-03-02 12:37:38 +0100

    "instances folder"

--- Zadanie1/instance_parser/euclidean_parser.py
+++ Zadanie1/instance_parser/euclidean_parser.py
@@ -3,5 +3,9 @@ from Zadanie1.instance_parser.instance_parser import Parser
 
 
 class EuclideanParser(Parser, ABC):
+    def open(self, tspfile):
+        for line in tspfile:
+            line = line.split(" ")
     def parse(self, filename=str):
-        return 0
+        pass
+
