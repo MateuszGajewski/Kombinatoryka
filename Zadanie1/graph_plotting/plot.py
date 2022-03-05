116bfb7b18672e8ad4b0a4f5936149ef28751f57
arabica <mg96272@gmail.com>
2022-03-05 20:23:00 +0100

    "Graph fix"

--- Zadanie1/graph_plotting/plot.py
+++ Zadanie1/graph_plotting/plot.py
@@ -12,7 +12,7 @@ class Plot:
 
         fig, ax = plt.subplots(1)  # Prepare 2 plots
         ax.set_title('Solutions ' + self.name)
-        ax.scatter(points[:, 1], points[:, 2], s = 1)  # plot A
+        ax.scatter(points[:, 1], points[:, 2])  # plot A
         for i in range(1, len(cyclea)):
             ax.plot([points[cyclea[i-1], 1], points[cyclea[i], 1]],
                     [points[cyclea[i-1], 2], points[cyclea[i], 2]], 'ro-')
@@ -20,8 +20,9 @@ class Plot:
                 [points[cyclea[0], 2], points[cyclea[-1], 2]], 'ro-')
 
         for i in range(1, len(cycleB)):
-            ax.plot([points[cycleB[i-1], 1], points[cycleB[i], 1]],
-                    [points[cycleB[i-1], 2], points[cyclea[i], 2]], 'bo-')
+            ax.plot([points[cycleB[i - 1], 1], points[cycleB[i], 1]],
+                    [points[cycleB[i - 1], 2], points[cycleB[i], 2]], 'bo-')
         ax.plot([points[cycleB[0], 1], points[cycleB[-1], 1]],
                 [points[cycleB[0], 2], points[cycleB[-1], 2]], 'bo-')
+
         plt.show()
--- Zadanie1/main.py
+++ Zadanie1/main.py
@@ -1,6 +1,7 @@
 from instance_parser import euclidean_parser
 from solvers.greedy_nearest_neighbor import GreedyNearestNeighbor
 from solvers.greedy_cycle import GreedyCycle
+from solvers.regret import RegretSolver
 from graph_plotting.plot import Plot
 
 p = euclidean_parser.EuclideanParser()
@@ -16,4 +17,10 @@ print("--- GREEDY CYCLE ---")
 gc_solver = GreedyCycle(matrix)
 cycles = gc_solver.solve()
 plot2 = Plot("c")
+plot2.draw(cycles, points)
+
+print("--- 2-REGRET CYCLE ---")
+r_solver = RegretSolver(matrix)
+cycles = r_solver.solve()
+plot2 = Plot("r")
 plot2.draw(cycles, points)
