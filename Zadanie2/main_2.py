a059e849f11834b9a80fb07f996d943f654e9a44
Vourer <51489464+Vourer@users.noreply.github.com>
2022-03-20 14:53:33 +0100

    Feature #8 - two distinct neighbourhoods
    Neigbourhood A was split into two distinct Neighbourhoods, in accordance with homework requirements.

--- Zadanie2/main_2.py
+++ Zadanie2/main_2.py
@@ -1,8 +1,8 @@
 from utils.instance_parser import euclidean_parser
 from Zadanie1.solvers.greedy_nearest_neighbor import GreedyNearestNeighbor
-from Zadanie1.solvers.greedy_cycle import GreedyCycle
 from utils.graph_plotting.plot import Plot
-from Zadanie2.solvers.neighbourhood_a import Neighbourhood_a
+from Zadanie2.solvers.neighbourhood_a import NeighbourhoodA
+from Zadanie2.solvers.neighbourhood_b import NeighbourhoodB
 from Zadanie2.solvers.greedy_local_search import GreedyLocalSolver
 import numpy as np
 
@@ -13,13 +13,23 @@ print("--- GREEDY LOCAL SEARCH ---")
 gc_solver = GreedyNearestNeighbor(matrix)
 cycles = gc_solver.solve()
 
-#cycles = [np.arange(0, 50, 1), np.arange(50, 100, 1)]
+# cycles = [np.arange(0, 50, 1), np.arange(50, 100, 1)]
 plot = Plot("nn")
 plot.draw(cycles, points)
-n = Neighbourhood_a(matrix, cycles[0], cycles[1])
-greedy_solver = GreedyLocalSolver(n)
-cycles = greedy_solver.solve()
-print(len(cycles[0]))
-print(len(cycles[1]))
-plot1 = Plot("greedy")
-plot1.draw(cycles, points)
+
+n = NeighbourhoodA(matrix, cycles[0], cycles[1])
+nB = NeighbourhoodB(matrix, cycles[0], cycles[1])
+
+a_greedy_solver = GreedyLocalSolver(n)
+a_cycles = a_greedy_solver.solve()
+print(len(a_cycles[0]))
+print(len(a_cycles[1]))
+plot1 = Plot("greedy A")
+plot1.draw(a_cycles, points)
+
+b_greedy_solver = GreedyLocalSolver(nB)
+b_cycles = b_greedy_solver.solve()
+print(len(b_cycles[0]))
+print(len(b_cycles[1]))
+plot2 = Plot("greedy B")
+plot2.draw(b_cycles, points)
--- Zadanie2/solvers/neighbourhood_b.py
+++ Zadanie2/solvers/neighbourhood_b.py
@@ -6,73 +6,41 @@ import numpy as np
 import random
 
 
-# Opcja z zamianą kolejności i między cyklami
-class NeighbourhoodA(Neighbourhood):
+# Opcja z zamianą krawędzi i wymianą wierzchołkami między cyklami
+class NeighbourhoodB(Neighbourhood):
 
-    def calc_swap_inside(self, i, j, cycle):
-        delta = 0
-
-        if (i == 0 and j == 49) or (i == 49 and j == 0):
-            if i == 0 and j == 49:
-                old = self.matrix[cycle[i]][cycle[(i + 1) % len(cycle)]] + \
-                      self.matrix[cycle[j]][cycle[(j - 1) % len(cycle)]]
-                new = self.matrix[cycle[j]][cycle[((i + 1) % len(cycle))]] + \
-                      self.matrix[cycle[i]][cycle[(j - 1) % len(cycle)]]
-                delta = -old + new
-
-            elif i == 49 and j == 0:
-                # print(i, j)
-
-                old = self.matrix[cycle[j]][cycle[(j + 1) % len(cycle)]] + \
-                      self.matrix[cycle[i]][cycle[(i - 1) % len(cycle)]]
-                new = self.matrix[cycle[i]][cycle[(j + 1) % len(cycle)]] + \
-                      self.matrix[cycle[j]][cycle[(i - 1) % len(cycle)]]
-                delta = -old + new
-            return delta
-        elif i != j and abs(i - j) != 1:
-            # print(i, j)
-
-            old = self.matrix[cycle[i]][cycle[(i - 1) % len(cycle)]] + \
-                  self.matrix[cycle[i]][cycle[(i + 1) % len(cycle)]] + \
-                  self.matrix[cycle[j]][cycle[(j - 1) % len(cycle)]] + \
-                  self.matrix[cycle[j]][cycle[(j + 1) % len(cycle)]]
-
-            new = self.matrix[cycle[j]][cycle[(i - 1) % len(cycle)]] + \
-                  self.matrix[cycle[j]][cycle[(i + 1) % len(cycle)]] + \
-                  self.matrix[cycle[i]][cycle[(j - 1) % len(cycle)]] + \
-                  self.matrix[cycle[i]][cycle[(j + 1) % len(cycle)]]
-            delta = -old + new
-            return delta
-        elif i != j:
-            if i < j:
-                # print(i, j)
-                old = self.matrix[cycle[i]][cycle[(i - 1) % len(cycle)]] + \
-                      self.matrix[cycle[j]][cycle[(j + 1) % len(cycle)]]
-                new = self.matrix[cycle[j]][cycle[(i - 1) % len(cycle)]] + \
-                      self.matrix[cycle[i]][cycle[(j + 1) % len(cycle)]]
-
-            else:
-                # print(i, j)
-                old = self.matrix[cycle[j]][cycle[(j - 1) % len(cycle)]] + \
-                      self.matrix[cycle[i]][cycle[(i + 1) % len(cycle)]]
-                new = self.matrix[cycle[i]][cycle[(j - 1) % len(cycle)]] + \
-                      self.matrix[cycle[j]][cycle[(i + 1) % len(cycle)]]
-            delta = -old + new
-        return delta
+    def calc_swap_inside(self, e1: Edge, e2: Edge, cycle):
+        old = e1.cost + e2.cost
+        new = self.matrix[cycle[e1.v1]][cycle[e2.v1]] + self.matrix[cycle[e1.v2]][cycle[e2.v2]]
+        return -old + new
 
     def get_moves_in_cycle(self, cycle, s1, s2, step, move_type):
-        # solution = [Move(który, z którym, jaka zmiana)]
-        solutions = []
-        # w ramach cyklu
+        moves = []
 
         for i in range(s1, np.sign(step) * (s1 + len(self.cycleA)), step):
             for j in range(s2, (s2 + len(self.cycleB)) * np.sign(step), step):
                 if i != j:
                     real_i = i % len(cycle)
-                    real_j = j % len(cycle)
-                    solutions.append(Move(real_i, real_j, self.calc_swap_inside(real_i, real_j, cycle), move_type))
+                    buddy_i = (i + 1 * np.sign(step)) % len(cycle)
+                    if real_i > buddy_i or (real_i == 0 and buddy_i == 49):
+                        if not (real_i == 49 and buddy_i == 0):
+                            real_i, buddy_i = buddy_i, real_i  # lower index should be first (unless 49--0), swap them
+                    e1 = Edge(real_i, buddy_i, self.matrix[cycle[real_i]][cycle[buddy_i]])
 
-        return solutions
+                    real_j = j % len(cycle)
+                    buddy_j = (j + 1 * np.sign(step)) % len(cycle)
+                    if real_j > buddy_j or (real_j == 0 and buddy_j == 49):
+                        if not (real_j == 49 and buddy_j == 0):
+                            real_j, buddy_j = buddy_j, real_j  # lower index should be first (unless 49--0), swap them
+                    e2 = Edge(real_j, buddy_j, self.matrix[cycle[real_j]][cycle[buddy_j]])
+
+                    if e1 == e2:
+                        continue  # somehow ended up with the same edge
+
+                    if e1.v1 > e2.v1:
+                        e1, e2 = e2, e1  # edges should be in the order of their appearance in cycle
+                    moves.append(Move(e1, e2, self.calc_swap_inside(e1, e2, cycle), move_type))
+        return moves
 
     def calc_swap_between(self, i, j):
         old = self.matrix[self.cycleA[i]][self.cycleA[((i - 1) % len(self.cycleA))]] + \
@@ -99,9 +67,9 @@ class NeighbourhoodA(Neighbourhood):
     def get_best_move(self):
         # wymiana wierzchołkami pomiędzy cyklami
         swap_moves = self.swap_between_cycles(0, 0, 1, move_type=MoveType.NODE_SWAP_BETWEEN_AB)
-        # zamiany wierzchołków w poszczególnych cyklach
-        cycleA_moves = self.get_moves_in_cycle(self.cycleA, 0, 0, 1, move_type=MoveType.NODE_SWAP_IN_A)
-        cycleB_moves = self.get_moves_in_cycle(self.cycleB, 0, 0, 1, move_type=MoveType.NODE_SWAP_IN_B)
+        # zamiany krawędzi w poszczególnych cyklach
+        cycleA_moves = self.get_moves_in_cycle(self.cycleA, 0, 0, 1, move_type=MoveType.EDGE_SWAP_IN_A)
+        cycleB_moves = self.get_moves_in_cycle(self.cycleB, 0, 0, 1, move_type=MoveType.EDGE_SWAP_IN_B)
 
         cycleA_deltas = np.asarray([move.delta for move in cycleA_moves], dtype=int)
         idA = np.argwhere(cycleA_deltas == np.min(cycleA_deltas))[0, 0]
@@ -138,19 +106,19 @@ class NeighbourhoodA(Neighbourhood):
                     return first_best_move
             elif r % n_options == 1:
                 i = random.randint(0, len(self.cycleA))
-                j = random.randint(0, len(self.cycleA))
+                j = random.randint(0, len(self.cycleB))
                 step = random.choice([-1, 1])
-                moves = np.asarray(self.get_moves_in_cycle(self.cycleA, i, j, step, move_type=MoveType.NODE_SWAP_IN_A))
+                moves = np.asarray(self.get_moves_in_cycle(self.cycleA, i, j, step, MoveType.EDGE_SWAP_IN_A))
                 deltas = np.asarray([move.delta for move in moves], dtype=int)
                 if any(deltas < 0):
                     improvements = moves[deltas < 0]
                     first_best_move = improvements[0]
                     return first_best_move
             elif r % n_options == 2:
-                i = random.randint(0, len(self.cycleB))
+                i = random.randint(0, len(self.cycleA))
                 j = random.randint(0, len(self.cycleB))
                 step = random.choice([-1, 1])
-                moves = np.asarray(self.get_moves_in_cycle(self.cycleB, i, j, step, move_type=MoveType.NODE_SWAP_IN_B))
+                moves = np.asarray(self.get_moves_in_cycle(self.cycleB, i, j, step, MoveType.EDGE_SWAP_IN_B))
                 deltas = np.asarray([move.delta for move in moves], dtype=int)
                 if any(deltas < 0):
                     improvements = moves[deltas < 0]
