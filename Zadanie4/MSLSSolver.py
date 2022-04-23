9551bb7aaaa69b357a816e7e0e363526515ff4dc
arabica <mg96272@gmail.com>
2022-04-23 11:36:32 +0200

    MLS I guess

--- Zadanie4/MSLSSolver.py
+++ Zadanie4/MSLSSolver.py
@@ -3,4 +3,28 @@ from Zadanie2.solvers.local_search_solver import LocalSearchSolver
 class MLSSolver(LocalSearchSolver):
 
     def solve(self):
-        pass
+        self.neighbourhood.generate_all_moves()
+
+        i = 0
+        while i < 100:
+            move = self.neighbourhood.get_best_move()
+            # print(move)
+
+            if move is not None and move.delta < 0:
+                made = self.neighbourhood.validate_move_and_make(move)
+                while made == -1:
+                    move = self.neighbourhood.get_best_move()
+                    if move is None or move.delta > 0:
+                        break
+                    made = self.neighbourhood.validate_move_and_make(move)
+
+                self.neighbourhood.update_moves(move)
+            else:
+                # no further improvements
+                break
+
+            i += 1
+            if i % 50 == 0:
+                print(f"Memory #{i}")
+
+        return [self.neighbourhood.cycleA, self.neighbourhood.cycleB]
---
+++ Zadanie4/main_4.py
@@ -0,0 +1,70 @@
+import time
+import numpy as np
+
+from Zadanie1.solvers.greedy_cycle import GreedyCycle
+from Zadanie1.solvers.random_solver import RandomSolver
+from utils.instance_parser import euclidean_parser
+from utils.graph_plotting.plot import Plot
+from Zadanie2.entity.move_type import MoveType
+from Zadanie2.entity.solution import Solution
+from Zadanie2.solvers.neighbourhood import Neighbourhood
+from Zadanie2.solvers.greedy_local_search import GreedyLocalSolver
+from Zadanie2.solvers.steep_local_search import SteepLocalSolver
+from Zadanie2.solvers.random_local_search import RandomLocalSearchSolver
+from Zadanie3.neighbourhood_opt import Neighbourhood_opt
+from Zadanie3.neighbourhood_candidate import NeighbourhoodCandidate
+from Zadanie3.opt_moves_list import OptLocalSolver
+from Zadanie3.candidate_solver import CandidateSolver
+from Zadanie4.MSLSSolver import MLSSolver
+
+
+def calculate_cycle_len(matrix, cycles):
+    total = 0
+    for cycle in cycles:
+        for i, node in enumerate(cycle):
+            total += matrix[cycle[i-1]][node]
+    return total
+
+def run():
+    p = euclidean_parser.EuclideanParser()
+    matrix = p.parse("../utils/instances/kroA200.tsp")
+    points = p.get_points()
+
+    a_moves = [MoveType.NODE_SWAP_IN_A, MoveType.NODE_SWAP_IN_B, MoveType.NODE_SWAP_BETWEEN_AB]
+    b_moves = [MoveType.EDGE_SWAP_IN_A, MoveType.EDGE_SWAP_IN_B, MoveType.NODE_SWAP_BETWEEN_AB]
+    c_moves = [MoveType.CANDIDATE_IN_A, MoveType.CANDIDATE_IN_B, MoveType.NODE_SWAP_BETWEEN_AB]
+    random_moves = [mt for mt in MoveType]
+
+    solutions = [
+        Solution("MLSS", MLSSolver, b_moves, Neighbourhood_opt),
+            ]
+
+    greedy_results = []
+    greedy_times = []
+
+    for i in range(0, 10):
+        print(f"Running iteration #{i}")
+        greedy_start = time.time()
+
+        instance = GreedyCycle(matrix, starting_point=None).solve()
+
+        duration = round(time.time() - greedy_start, 3)
+        greedy_times.append(duration)
+        greedy_results.append(calculate_cycle_len(matrix, instance))
+        print(f"Instance #{i} is ready")
+
+        for solution in solutions:
+            cycles = instance.copy()
+            solution.find(matrix, cycles)
+
+    for solution in solutions:
+        print(solution)
+
+        plot = Plot(solution.solver_name)
+        plot.draw(solution.best_instance, points)
+
+
+if __name__ == "__main__":
+    run()
+else:
+    print("IMO3")
